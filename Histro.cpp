c4a43f272554864e464bbd2a037bcfca93caf4fa
AH Fahim <36567376+AHfahim40@users.noreply.github.com>
2018-03-31 00:27:59 +0600

    Add files via upload

--- Histro.cpp
+++ Histro.cpp
@@ -6,7 +6,7 @@ using namespace std;
         public:
         int display()
         {
-            cout<<"Historical Place in BD"<<endl;
+            cout<<"Historical Place in BD\n"<<endl;
 
         }
 
@@ -21,33 +21,63 @@ class Mahastangarh : public Bangladesh
     int distance;
 
 int display(){
-    cout<<"Mahastangarh\n";
+    cout<<"\nMahastangarh\n";
     cout<<"Cost is 30 tk\n";
     cout<<"Time is 45 miniutes\n";
     cout<<"Distance is 13 Km\n";
 }
 };
 
-class Shalban Vihara: public Bangladesh
+class ShalbanVihara : public Bangladesh
 
 {
 
+public :
     int display(){
-    cout<<"Shalban Vihara\n";
+    cout<<"\nShalban Vihara\n";
     cout<<"Cost is 25 tk\n";
     cout<<"Time is 33 miniutes\n";
     cout<<"Distance is 10 Km\n";
 }
+};
 
 
         int main()
         {
-            Bangladesh b1;
-            Mahastangarh m1;
-            Shalban Vihara s1;
-            b1.display();
-            m1.display();
-            s1.display();
+            int ch;
+            char Ch1;
+            Bangladesh bb;
+
+
+        cout<<"******* Welcome ******"<<endl;
+        bb.display();
+        do{
+            cout<<"\nPlease enter your choice "<<endl;
+            cout<<"1 for Mahastan garh "<<endl;
+            cout<<"2 for Shalban Vihara"<<endl;
+            cout<<"0 for exit"<<endl;
+            cin>>ch;
+            cout<<"You entered "<<ch<<endl;
+            switch(ch){
+            case 1 :
+                 Mahastangarh m1;
+                  m1.display();
+                  break;
+
+            case 2:
+
+                ShalbanVihara s1;
+                s1.display();
+                break;
+
+            case 0 :
+                return 0;
+            }
+            cout<<"Do you went to continue (Y/N) : ";
+            cin>>Ch1;
+
+        }while(Ch1 == 'Y' || Ch1 == 'y');
+
             return 0;
 
         }
