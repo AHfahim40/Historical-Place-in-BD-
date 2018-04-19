aac2c4caab508db8b8686ef3514f3684effc0537
AH Fahim <36567376+AHfahim40@users.noreply.github.com>
2018-04-20 00:45:45 +0600

    Add files via upload

--- Histro.cpp
+++ Histro.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-//#include<graphics.h>
+//include<graphics.h>
 
 #include<stdlib.h>
 
@@ -47,7 +47,7 @@ public :
     int display(){
 
     cout<<"\nShalban Vihara\n";
-    cout<<"Cost is 40 tk from Comilla\n";
+    cout<<"Cost is 20 tk from Comilla\n";
     cout<<"Time is 33 miniutes from Comilla\n";
     cout<<"Distance is 10 Km from Comilla\n";
     cout<<"Discover: Before 1200 years ago the King Bhava Deva\n";
@@ -70,9 +70,9 @@ class SomapuraMahavihara : public Bangladesh
 public :
     int display(){
     cout<<"\nSomapura Mahavihara\n";
-    cout<<"Cost is 100 tk\n";
-    cout<<"Time is 1 h 8 min\n";
-    cout<<"Distance is 32.0 km\n";
+    cout<<"Cost is 60 tk from Naogaon\n";
+    cout<<"Time is 1 h 8 min from Naogaon\n";
+    cout<<"Distance is 32.0 km from Naogaon\n";
     cout<<"Discover: A number of monasteries grew up during the Pāla period in ancient Bengal and Magadha\n";
     cout<<"          According to Tibetan sources, five great Mahaviharas stood out\n";
     cout<<"          Vikramashila, the premier university of the era; Nalanda\n";
@@ -82,6 +82,153 @@ public :
     cout<<"                   Keerti' Statue of Clay Stone,Damaged 'Haargouri' Statue\n";
     cout<<"                   Visnu' Statue of Krishna Stone,'Mansha' Statue of Clay Stone\n";
     cout<<"                   Uma' Statue of krishna Stone,Broken Statue of Laxmi Narayan of Krishna Stone\n";
+
+}
+};
+
+class NandailDighi : public Bangladesh
+{
+
+public :
+    int display(){
+    cout<<"\nNandail Dighi\n";
+    cout<<"Cost is 10 tk from Kalai,Joypurhat\n";
+    cout<<"Time is 15 min from Kalai,Joypurhat\n";
+    cout<<"Distance is 4.0 km from Kalai,Joypurhat\n";
+    cout<<"Discover: Nandail Dighi is huge and its length is 1 km\n";
+    cout<<"          It is said that King Nandail in 1610 excavated this Dighi\n";
+    cout<<"          The volume of this Dighi is 59,4 acre\n";
+    cout<<"          It is said by many that this Dighi was excavated in one night\n";
+    cout<<"\nHistorical place : Only Nandail Dighi is historical place here\n";
+
+
+}
+};
+
+class HindaKasbaShahiMosque : public Bangladesh
+{
+
+public :
+    int display(){
+    cout<<"\nHinda Kasba Shahi Mosque\n";
+    cout<<"Cost is 30 tk from Joypurhat\n";
+    cout<<"Time is 38 min from Joypurhat\n";
+    cout<<"Distance is 18.6 km from Joypurhat\n";
+    cout<<"Discover:  Hinda Kasba Shahi Mosque is a 55 years old mosque built during the 1365\n";
+    cout<<"          A Pir(a saint) named Hajrat Abu Gofur Chishti ordered to built this mosque here\n";
+    cout<<"          He himself was the designer of the mosque\n";
+    cout<<"          It is said by many that this Dighi was excavated in one night\n";
+    cout<<"\nHistorical place : Only Hinda Kasba Shahi Mosque is historical place here\n";
+
+
+}
+};
+
+class TeestaBarrage : public Bangladesh
+{
+
+public :
+    int display(){
+    cout<<"\nTeesta Barrage\n";
+    cout<<"Cost is 80 tk from Rangpur\n";
+    cout<<"Time is 1 h 48 min from Rangpur\n";
+    cout<<"Distance is 66.1 km from Rangpur\n";
+    cout<<"Discover: \n";
+    cout<<"          For producing more crops by providing the irrigation facilities in arable land of north locality\n";
+    cout<<"          of greater Rangpur, Dinajpur, and bogra district Government of that time in 1937 adopted the plan\n";
+    cout<<"          Engineers of the BWDB and experts of  BUET jointly reviewed the previous\n";
+    cout<<"          reports and prepared a detailed engin design\n";
+    cout<<"          The formation of the Tista Barrage started in 1979\n";
+    cout<<"          and that of the irrigation canals in 1984, and was completed in 1997-1990\n";
+    cout<<"          President H. M. Ershad officially inaugurated the Tista barrage\n";
+    cout<<"\nHistorical place : Only Teesta Barrage is historical place here\n";
+
+
+}
+};
+
+class LalbaghFort: public Bangladesh
+{
+
+public :
+    int display(){
+    cout<<"\nLalbagh Fort\n";
+    cout<<"Cost is 20 tk from Dhaka\n";
+    cout<<"Time is 46 min from Dhaka\n";
+    cout<<"Distance is 13.6 km from Dhaka\n";
+    cout<<"Discover: Lalbagh Fort (also Fort Aurangabad) is an incomplete 17th century Mughal fort complex that stands\n";
+    cout<<"          before the Buriganga River in the southwestern part of Dhaka, Bangladesh\n";
+    cout<<"           The construction was started in 1678 AD by Mughal Subahdar Muhammad Azam Shah\n";
+    cout<<"          who was son of Emperor Aurangzeb and later emperor himself\n";
+    cout<<"          The fort was never completed, and unoccupied for a long period, so that much\n";
+    cout<<"           of what was there was cleared or built over, and the modern restored and preserved\n";
+    cout<<"           site now runs up to modern buildings at its edges\n";
+    cout<<"\nHistorical place : A water tank,Tomb of Bibi Pari,Lalbagh Fort Mosque\n";
+
+}
+};
+
+
+class CoxBazar: public Bangladesh
+{
+
+public :
+    int display(){
+    cout<<"\nCox's Bazar\n";
+    cout<<"Cost is 250 tk from Chittagong\n";
+    cout<<"Time is 4 h 39 min from Chittagong\n";
+    cout<<"Distance is 147 km from Chittagong\n";
+    cout<<"Discover: Cox's Bazar is a city,fishing port,tourism centre and district headquarters in Bangladesh\n";
+    cout<<"          When the Mughal Prince Shah Shuja was passing through the hilly terrain\n";
+    cout<<"           The construction was started in 1678 AD by Mughal Subahdar Muhammad Azam Shah\n";
+    cout<<"          Shuja was passing through the hilly terrain of the present-day Cox's Bazar on his way to Arakan\n";
+    cout<<"          The was attracted to its scenic and captivating beauty\n";
+    cout<<"           The name Cox's Bazar originated from the name of a British East India Company officer\n";
+    cout<<"           officer, Captain Hiram Cox, who was appointed as the Superintendent of Palonki\n";
+    cout<<"           (today's Cox's Bazar) outpost\n";
+    cout<<"\nHistorical place : Himchari National Park,Aggmeda Khyang,Ramu\n";
+    cout<<"                     Bangabandhu Sheikh Mujib Safari Park,Inani Beach\n";
+
+}
+};
+
+class SajekValley: public Bangladesh
+{
+
+public :
+    int display(){
+    cout<<"\nSajek Valley\n";
+    cout<<"Cost is 200 tk from Rangamati\n";
+    cout<<"Time is 4 h 15 min from Rangamati\n";
+    cout<<"Distance is 133 km from Rangamati\n";
+    cout<<"Discover: Sajek Valley is an emerging tourist spot in Bangladesh situated among the hills of Kasalong range\n";
+    cout<<"          of mountains in Sajek union, Baghaichhari Upazila in Rangamati District\n";
+    cout<<"          The valley is 1800 feet above sea level\n";
+    cout<<"          Sajek valley is known as the Queen of Hills & Roof of Rangamati\n";
+    cout<<"          The name of Sajek Valley came from Sajek River that originates from Karnafuli river\n";
+    cout<<"           Sajek river works as a border between Bangladesh and India\n";
+    cout<<"\nHistorical place :  Mountains,Dense Forest,Grasslands Hilly Tracks\n";
+    cout<<"                     Kachalong,Machalong,Mayni River\n";
+
+}
+};
+
+class Jaflong: public Bangladesh
+{
+
+public :
+    int display(){
+    cout<<"\nJaflong\n";
+    cout<<"Cost is 100 tk from Sylhet\n";
+    cout<<"Time is 2 h 13 min from Sylhet\n";
+    cout<<"Distance is 56.8 km from Sylhet\n";
+    cout<<"Discover: Jaflong is a hill station and popular tourist destination in the Division of Sylhet, Bangladesh\n";
+    cout<<"           It is located in Gowainghat Upazila of Sylhet District and situated at the border\n";
+    cout<<"           between Bangladesh and the Indian state of Meghalaya,\n";
+    cout<<"           overshadowed by subtropical mountains and rainforests\n";
+    cout<<"          Jaflong is famous for its stone collections and is home of the Khasi tribe\n";
+    cout<<"\nHistorical place :  Khasia Rajbari,Dauki and Piyain Rivers\n";
+    cout<<"                     Orange and jackfruit gardens,Betel leaf and areca nut gardens,Dauki Bazar\n";
 
 }
 };
@@ -99,9 +246,17 @@ public :
         bb.display();
         do{
             cout<<"\nPlease enter your choice "<<endl;
-            cout<<"1 for Mahastan garh "<<endl;
+            cout<<"1 for Mahastan Garh "<<endl;
             cout<<"2 for Shalban Vihara"<<endl;
             cout<<"3 for Somapura Mahavihara"<<endl;
+            cout<<"4 for Nandail Dighi"<<endl;
+            cout<<"5 for Hinda Kasba Shahi Mosque"<<endl;
+            cout<<"6 for Tista Barrage"<<endl;
+            cout<<"7 for Lalbagh Fort"<<endl;
+            cout<<"8 for Cox's  Bazar"<<endl;
+            cout<<"9 for Sajek Valley"<<endl;
+            cout<<"10 for Jaflong"<<endl;
+
             cout<<"0 for exit"<<endl;
             cin>>ch;
             cout<<"You entered "<<ch<<endl;
@@ -117,11 +272,50 @@ public :
                 ShalbanVihara s1;
                 s1.display();
                 break;
+
             case 3 :
 
                 SomapuraMahavihara k1;
                 k1.display();
                 break;
+
+                case 4 :
+
+                NandailDighi d1;
+                d1.display();
+                break;
+
+                  case 5 :
+
+                 HindaKasbaShahiMosque h1;
+                h1.display();
+                break;
+
+                case 6 :
+                TeestaBarrage t1;
+                t1.display();
+                break;
+
+             case 7 :
+               LalbaghFort l1;
+                l1.display();
+                break;
+
+                case 8 :
+                CoxBazar c1;
+                c1.display();
+                break;
+
+                case 9 :
+                SajekValley v1;
+                v1.display();
+                break;
+
+                case 10 :
+                Jaflong j1;
+                j1.display();
+                break;
+
 
             case 0 :
 
